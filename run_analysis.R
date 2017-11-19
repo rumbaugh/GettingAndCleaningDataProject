53b60346629a4a82dc52f5db6e1cf467328211ce
Nick Rumbaugh <takkyon13@gmail.com>
2017-11-18 22:44:29 -0800

    Added documentation

--- run_analysis.R
+++ run_analysis.R
@@ -1,16 +1,31 @@
 run_analysis <- function(UCIdatasetpath = './UCI HAR Dataset'){
+    ## First, load activities key file
     activities_key_df <- read.table(paste(UCIdatasetpath, '/activity_labels.txt', sep = ''), col.names = c('ActivityID', 'Activity'))
+    ## Load file containing feature names
+    feature_name_df <- read.table(paste(UCIdatasetpath, '/features.txt', sep = ''), col.names = c('ColNum', 'Feature'), as.is = T)
+    ## Pick out columns with means or standard deviations
+    mean_or_std_inds <- grep('.*[Mm][Ee][Aa][Nn].*|.*[Ss][Tt][Dd].*', feature_name_df$Feature)
+    features <-  feature_name_df$Feature[mean_or_std_inds]
+    ## Set up character vector for load_features that will pick out
+    ## the right columns
+    X_classes <- c(rep("NULL", dim(feature_name_df)[1]))
+    X_classes[mean_or_std_inds] = "numeric"
 
     load_features <- function(set = 'test'){
-        feature_name_df <- read.table(paste(UCIdatasetpath, '/features.txt', sep = ''), col.names = c('ColNum', 'Feature'), as.is = T)
-    	mean_or_std_inds <- grep('.*[Mm][Ee][Aa][Nn].*|.*[Ss][Tt][Dd].*', feature_name_df$Feature)
-    	features <-  feature_name_df$Feature[mean_or_std_inds]
-    	X_classes <- c(rep("NULL", dim(feature_name_df)[1]))
-    	X_classes[mean_or_std_inds] = "numeric"
+        ### This function loads one of the files in the dataset that
+	### contain features. There are two, which can be chosen by
+	### setting 'set' to either 'test' or 'train'.
+
+	## Load data frame from features file
     	X_df <- read.table(paste(UCIdatasetpath, '/', set, '/X_', set, '.txt', sep = ''), colClasses = X_classes, as.is = T, col.names = feature_name_df$Feature)
+	## Load subjects file
     	subject_df <- read.table(paste(UCIdatasetpath, '/', set, '/subject_', set, '.txt', sep = ''), as.is = T, col.names = c('SubjectID'))
+	## Add subjects to features data frame
     	X_df$SubjectID <- subject_df$SubjectID
+	## Load activities file 
     	activities_df <- read.table(paste(UCIdatasetpath, '/', set, '/y_', set, '.txt', sep = ''), as.is = T, col.names = c('ActivityID'))
+	## Create column where activity numeric IDs are replaced with 
+	## the activity names
     	activities_df$rowID <- 1:nrow(activities_df)	      
     	activities_df <- merge(activities_df, activities_key_df, sort = F)
     	activities_df <- activities_df[order(activities_df$rowID), ]
@@ -18,12 +33,15 @@ run_analysis <- function(UCIdatasetpath = './UCI HAR Dataset'){
     	X_df
     }
     
+    ## Load the test and train feature files and merge the 
+    ## two dataframes together
     test_df <- load_features('test')
     train_df <- load_features('train')
     df <- rbind(test_df, train_df)
 }
 
 write_analysis <- function(outputfile, UCIdatasetpath = './UCI HAR Dataset'){
+    ## Run run_analysis and write the output data frame to a file
     outdf <- run_analysis(UCIdatasetpath = UCIdatasetpath)
     write.table(outdf, outputfile, row.names = F)
 }
